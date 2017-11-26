f57b99f888767b39aef7ada898278e760563f473
Yuliia <skylenko@gmail.com>
2017-11-27 00:17:49 +0200

    BinTree put method implemented

---
+++ BinTree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class BinTree
+    {
+        private Node root;
+        
+        public void put(int obj)
+        {
+            
+            Node currentNode = new Node();
+            currentNode.element = obj;
+            
+
+            
+            if (root == null)
+            {
+                root = currentNode;
+                return;
+            }
+
+            Node temp = root;
+            
+            Node parent = temp;
+            while (temp != null)
+            {
+                parent = temp;
+                if (currentNode.CompareTo(temp) < 0)
+                {
+                    temp = temp.left; 
+                } else if (currentNode.CompareTo(temp) > 0)
+                {
+                    temp = temp.right;
+                }
+           
+            }
+
+            if (currentNode.CompareTo(parent) < 0)
+            {
+                parent.left = currentNode;
+            } else if (currentNode.CompareTo(parent) > 0)
+            {
+                parent.right = currentNode;
+            }
+        }
+
+    }
+
+    internal class Node : IComparable
+    {
+        internal int element;
+        internal Node left;
+        internal Node right;
+
+        public int CompareTo(object obj)
+        {
+            return element - (int)obj;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinTree binTree = new BinTree();
+            binTree.put(5);
+            binTree.put(1);
+            binTree.put(9);
+            
+            Console.WriteLine(binTree);
+            Console.ReadLine();
+        }
+    }
+}
